da77a14ec9376638bb84f03fbc9a0f554e56caa2
miaoerduo <zhaoyu3210@foxmail.com>
2019-03-07 02:07:21 +0800

    support no results

---
+++ easy_pipeline/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from .task import Task, EmptyTask, StopTask
+from .worker import Worker, SimpleWorker, SimpleWorkerProcess
+from .pipeline import Pipeline, PipelineItem, SimplePipeline
--- main.py
+++ main.py
@@ -3,59 +3,62 @@ from easy_pipeline import SimplePipeline, PipelineItem, Task, StopTask, EmptyTas
 import multiprocessing as mp
 
 
-# 定义自己的Task
+# define our Task
 class NumTask(Task):
     def __init__(self, x):
+        super(NumTask, self).__init__()
         self.val = x
 
 
-# 初始化函数
+# init function, here we use closure to get different function
 def get_init_fn(x):
     def init():
         return x
     return init
 
 
+# operations
 def plus(res, task):
-    result = task.val + res
-    return NumTask(result)
+    return NumTask(task.val + res)
 
 
 def mul(res, task):
-    result = task.val * res
-    return NumTask(result)
+    return NumTask(task.val * res)
 
 
 def minus(res, task):
-    result = task.val - res
-    return NumTask(result)
+    return NumTask(task.val - res)
+
+
+def div(res, task):
+    return NumTask(task.val / res)
 
 
 if __name__ == '__main__':
 
-    mp.freeze_support()
+    # job queue
+    manager = mp.Manager()
+    job_queue = manager.Queue(1000)
 
-    # x = (x + 1) * 2 - 3
+    # define pipeline and start
+
+    # x = （(x + 1) * 2 - 3）/ 5
     pipeline_items = [
         PipelineItem(plus, get_init_fn(1), 1, 10),      # plus 1
         PipelineItem(mul, get_init_fn(2), 1, 10),       # mul 2
         PipelineItem(minus, get_init_fn(3), 1, 10),     # minus 3
+        PipelineItem(div, get_init_fn(5.), 1, 10),      # div 5
     ]
 
-    manager = mp.Manager()
-    job_queue = manager.Queue(1000)
-
-    # 构建Pipeline 并 启动
     pipeline = SimplePipeline(pipeline_items, job_queue)
     pipeline.start()
+    result_queue = pipeline.get_result_queue()
 
-    # Feed Task
+    # Feed jobs anytime (before StopTask)
     for i in range(100):
         job_queue.put(NumTask(i))
-    job_queue.put(StopTask())
 
-    # 获取输出
-    result_queue = pipeline.get_result_queue()
+    # get output
     for i in range(100):
         result = result_queue.get()
         if isinstance(result, StopTask):
@@ -63,3 +66,9 @@ if __name__ == '__main__':
         if isinstance(result, EmptyTask):
             continue
         print(result.val)
+
+    # stop all processes
+    # the StopTask number is usually the same as workers
+    for p in pipeline_items:
+        for _ in range(p.worker_num):
+            job_queue.put(StopTask())
